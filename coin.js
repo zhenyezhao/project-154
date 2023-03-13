4db4b9e14c9ff303083a419e56f039b910eb2a20
zhenyezhao <86699069+zhenyezhao@users.noreply.github.com>
2023-03-12 19:04:34 -0700

    Add files via upload

---
+++ coin.js
@@ -0,0 +1,10 @@
+AFRAME .registerComponent('coins')
+init: function(){
+    for (var i=1; i<=10; i++)(
+        const id=coin$(1)
+
+        const posX=Math.random()*100+50;
+        const posY=Math.random()*5+5;
+        const posZ=Math.random()*60+60
+    )
+}
